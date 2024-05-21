6b20e0596d4fc031a80170c15828548a09e01ea0
w <walter.liao@monash.edu>
2024-05-21 17:14:19 +1000

    done

--- step2.py
+++ step2.py
@@ -32,31 +32,33 @@ def main():
     with open('D_x.npy','rb') as f:
         D_x_matrix = np.load(f)
     K_leaf = [e for e in range(D_x_matrix.shape[0])]    
-    d_all = convert_D_to_dict(D_matrix=D_x_matrix, init_K_leaf=K_leaf)
-    
+    d_old = convert_D_to_dict(D_matrix=D_x_matrix, init_K_leaf=K_leaf)
+    d_new = {}
     
     step = 0
     print(f"Initial K_leaf: {K_leaf}")
     
-    while len(K_leaf)==1:
+    while len(K_leaf)>2:
         
         step +=1
         print(f"Iteration {step}")
-        r = net_divergence(d_all, K_leaf)
-        D_adjusted = adjusted_distance(d_all, K_leaf, r)
+        r = net_divergence(d_old, K_leaf)
+        D_adjusted = adjusted_distance(d_old, K_leaf, r)
         
         
         pair_min = min(D_adjusted, key=D_adjusted.get)
-        d_all, K_leaf = distance_from_child_to_parent(p=pair_min, D=D_adjusted, r=r, K_leaf=K_leaf, d_all=d_all)
+        d_old, d_new, K_leaf = distance_from_child_to_parent(p=pair_min, D=D_adjusted, r=r, K_leaf=K_leaf, d_old=d_old, d_new=d_new)
         print(f"K_leaf is {K_leaf}")
-        
-
-    print(K_leaf)
     
     
-      
+    # when only 2 leaf nodes lefted in K_leaf, then we connect an edge between them using their distance
+    # apend this distance into the d_new dictionary:
+    d_new.update(d_old)
+    root_id = max(K_leaf) # the root node ID is the largest lb number in K_leaf
     
-    plot_tree(d_all)
+    
+    
+    plot_tree(d_new, root_id)
     
     
     
@@ -113,7 +115,7 @@ def adjusted_distance(D, K_leaf, r):
     return D_adjusted
 
 
-def distance_from_child_to_parent(p, D, r, K_leaf, d_all):
+def distance_from_child_to_parent(p, D, r, K_leaf, d_old, d_new):
     # 1. build 2 new branches from two identified child nodes (picked_pair p) linking to a same parent node, 
     #    assign this parent with a new label 
     # 2. remove the child node identified in (picked_pair p)
@@ -133,10 +135,7 @@ def distance_from_child_to_parent(p, D, r, K_leaf, d_all):
     K_leaf.append(new_parent_lb)
     K_leaf.remove(p[0])
     K_leaf.remove(p[1])
-    # remove edges in d_all that link to the picked_pair children nodes
-    for key in d_all:
-        if key == p or key == (p[1],p[0]):
-            del d_all[key]
+    
     
         
     # update the distance matrix by removing the picked pair (2 leaf nodes) 
@@ -146,8 +145,8 @@ def distance_from_child_to_parent(p, D, r, K_leaf, d_all):
     - Compute distance from child (removed pairs) to parent (newly created intermediate node)
     - For removed pair <i,j> --> use d(ik) = [d(ij) + r(i) - r(j)] / 2    &   d(jk) = [d(ij) + r(j) - r(i)] / 2
     '''    
-    d_all[(p[0], new_parent_lb)] = (D[(p[0],p[1])] + r[p[0]] - r[p[1]])/2
-    d_all[(p[1], new_parent_lb)] = (D[(p[0],p[1])] + r[p[1]] - r[p[0]])/2
+    d_new[(p[0], new_parent_lb)] = (D[(p[0],p[1])] + r[p[0]] - r[p[1]])/2
+    d_new[(p[1], new_parent_lb)] = (D[(p[0],p[1])] + r[p[1]] - r[p[0]])/2
     
     
     '''
@@ -159,17 +158,21 @@ def distance_from_child_to_parent(p, D, r, K_leaf, d_all):
     for i in K_leaf:
         # compute i's net divergence w.r.t. all other nodes, (a,b) and (b,a) are distinctly counted
         if i != new_parent_lb:
-            d_all[(new_parent_lb, i)] = (d_all[(p[0], i)] + d_all[(p[1], i)] - d_all[p])/2
-            d_all[(i, new_parent_lb)] = d_all[(new_parent_lb, i)]
+            d_old[(new_parent_lb, i)] = (d_old[(p[0], i)] + d_old[(p[1], i)] - d_old[p])/2
+            d_old[(i, new_parent_lb)] = d_old[(new_parent_lb, i)]
     
-    
-    return d_all, K_leaf
+    # remove edges in d_all that link to the picked_pair children nodes
+    for key in list(d_old.keys()):
+        if key[0] in p or key[1] in p:
+            del d_old[key]
+            
+    return d_old, d_new, K_leaf
 
 
 
 
 
-def plot_tree(d_all):
+def plot_tree(d_all, root_id):
     # refer: https://networkx.org/documentation/stable/auto_examples/drawing/plot_weighted_graph.html#sphx-glr-auto-examples-drawing-plot-weighted-graph-py
     G = nx.Graph()
     
@@ -180,10 +183,13 @@ def plot_tree(d_all):
 
 
 
-    pos = nx.spring_layout(G, seed=7)  # positions for all nodes - seed for reproducibility
+    # pos = nx.spring_layout(G, seed=7)  # positions for all nodes - seed for reproducibility
+    pos = nx.planar_layout(G)  # positions for all nodes - seed for reproducibility
+
 
     # nodes
     nx.draw_networkx_nodes(G, pos, node_size=700)
+    nx.draw_networkx_nodes(G, pos, nodelist=[root_id], node_color="tab:red")
 
     # edges
     nx.draw_networkx_edges(G, pos, edgelist=e, width=2)
